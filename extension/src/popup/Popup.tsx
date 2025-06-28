c4edbdae4745c0592141cc28a43632450bdffa03
Vivek Aithal <vivekaithal44@gmail.com>
2025-06-28 16:52:54 +0530

    edit popup content (#201)

--- extension/src/popup/Popup.tsx
+++ extension/src/popup/Popup.tsx
@@ -4,12 +4,14 @@ import { BsTerminalFill, BsTools } from "react-icons/bs";
 import { TbWorldShare } from "react-icons/tb";
 import { get } from 'lodash'
 
-const playgroundLink = 'https://minusx.ai/playground'
-const requestToolLink = 'https://minusx.ai/tool-request'
+// const playgroundLink = 'https://minusx.ai/playground'
+// const requestToolLink = 'https://minusx.ai/tool-request'
 const websiteLink = 'https://minusx.ai'
+const docsLink = 'https://docs.minusx.ai/en/collections/10790008-minusx-in-metabase'
+const blogLink = 'https://minusx.ai/blog'
 
 export const Popup: React.FC = () => {
-  const defaultPopupText = `We currently support MinusX on any Jupyter (Lab, Notebook, Lite, etc), or Metabase (SQL Query page) instance. You can test-drive it in a playground we've created for you, or request support for your favourite analytics apps!`
+  const defaultPopupText = `We currently support MinusX on all Metabase pages (SQL Query, Question Builder, Dashboard pages). You can test-drive it on your company Metabase, or on our website.`
   const [popupText, setPopupText] = React.useState<string>(defaultPopupText);
   useEffect(() => {
     chrome.storage.local.get().then((localConfigs) => {
@@ -30,8 +32,8 @@ export const Popup: React.FC = () => {
         </HStack>
         <Text bg={'gray.50'} borderRadius={5} p={5} mt={2} fontSize="sm">{popupText}</Text>
         <HStack justifyContent={"space-between"}>
-          <Button mt={2} onClick={() =>  window.open(playgroundLink, '_blank')} aria-label="Go to Playground" leftIcon={<BsTerminalFill/>} colorScheme='blue' variant='solid'>Go to Playground</Button>
-          <Button mt={3} onClick={() =>  window.open(requestToolLink, '_blank')} aria-label="Request Tool" leftIcon={<BsTools/>} colorScheme='gray' variant='solid'>App Support</Button>
+          <Button mt={2} onClick={() =>  window.open(docsLink, '_blank')} aria-label="View Docs" leftIcon={<BsTerminalFill/>} colorScheme='blue' variant='solid'>View Docs</Button>
+          <Button mt={3} onClick={() =>  window.open(blogLink, '_blank')} aria-label="Blog" leftIcon={<BsTools/>} colorScheme='gray' variant='solid'>New Features</Button>
         </HStack>
       </Box>
     </ChakraProvider>
